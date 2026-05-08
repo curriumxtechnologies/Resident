74b708b89383e6541a4ecfe7fb7dc77ae98cbf37
flameztech01 <flameztech01@gmail.com>
2026-05-08 11:43:24 +0100

    new

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ app.use(cors({
     origin: [
         'http://127.0.0.1:5500',
         'http://localhost:5500',
-        'http://localhost:5173'
+        'http://localhost:5173',
+        'https://www.rezidenthomes.com'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
